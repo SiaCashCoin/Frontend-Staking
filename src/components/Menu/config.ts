f6bb08530aa882f5b56f2041722c651da95adb10
SiaCashCoin <39414933+SiaCashCoin@users.noreply.github.com>
2021-12-17 18:06:31 -0500

    Update config.ts

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -36,6 +36,23 @@ const config: MenuEntry[] = [
     href: 'https://bulksend.siacashcoin.com',
   },
   {
+     label: 'LaunchPad',
+    icon: 'InfoIcon',
+    items: [
+       {
+        label: 'Create Token',
+        href: 'https://createtoken.siacaashcoin.com',
+       },
+       {
+        label: 'Safemoon Fork',
+         href: 'https://createsafemoon.siacashcoin.com',
+       },
+       {
+  
+    },
+   ],
+  },
+  {
     label: 'Games',
     icon: 'MoreIcon',
     href: 'https://tokenpvp.com/tokens/0xc26EaFC627624baDf990f8d30116892eD204DB51',
